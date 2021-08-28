517d4898f33be5c1df9bcc9925545b1812bd9af5
Nikita <nikitademedyuk1@gmail.com>
2021-08-28 15:24:14 +0300

    chat application

---
+++ Chat/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Chat
+//
+//  Created by Никита on 23.08.21.
+//
+
+import Foundation
+import Firebase
+
+struct Constants
+{
+    struct refs
+    {
+        static let databaseRoot = Database.database().reference()
+        static let databaseChats = databaseRoot.child("chats")
+    }
+}
